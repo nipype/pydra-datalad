5baaa2e9581c105b861722d539e0e30de40d7656
yibeichan <yibeichen@ucsb.edu>
2023-03-19 20:00:24 -0700

    revise code

--- pydra/tasks/datalad/interface/datalad.py
+++ pydra/tasks/datalad/interface/datalad.py
@@ -1,6 +1,4 @@
-"""A :obj:`~nipype.interfaces.utility.base.IdentityInterface` with a grafted Datalad getter."""
 import os
-import logging
 import typing as ty
 from pathlib import Path
 from pydra.engine.specs import (
@@ -13,48 +11,16 @@ from pydra.engine.core import TaskBase
 from pydra.engine.helpers import output_from_inputfields
 from pydra.utils.messenger import AuditFlag
 
-# logger = logging.getLogger("pydra.tasks.datalad")
-
 input_fields = [
-    (
-        "in_file",
-        str,
-        {
-            "help_string": "Path to the data to be downloaded through datalad",
-            "mandatory": True,
-        },
-    ),
-    (
-        "dataset_path",
-        Directory,
-        {
-            "help_string": "Path to the dataset that will be used to get data",
-            "mandatory": True,
-        },
-    ),
-    (
-        "dataset_url",
-        str,
-        {
-            "help_string": "URL to the dataset that will be used to get data",
-        },
-    ),
+    ("in_file", str, {"help_string": "Path to the data to be downloaded through datalad", "mandatory": True}),
+    ("dataset_path", Directory, {"help_string": "Path to the dataset that will be used to get data", "mandatory": True}),
+    ("dataset_url", str, {"help_string": "URL to the dataset that will be used to get data"}),
 ]
-
 
 output_fields = [
-    (
-        "out_file",
-        File,
-        {
-            "help_string": "file downloaded through datalad",
-            "requires": ["in_file"],
-            "output_file_template": "{in_file}",
-        },
-    )
+    ("out_file", File, {"help_string": "file downloaded through datalad", "requires": ["in_file"], "output_file_template": "{in_file}"}),
 ]
 
-# define a TaskBase calss
 class DataladInterface(TaskBase):
     """A :obj:`~nipype.interfaces.utility.base.IdentityInterface` with a grafted Datalad getter."""
 
@@ -74,13 +40,10 @@ class DataladInterface(TaskBase):
     ):
         """Initialize a DataladInterface instance."""
 
-        self.input_spec = input_spec or SpecInfo(
-            name="Inputs", fields=input_fields, bases=(BaseSpec,)
-        )
-        self.output_spec = output_spec or SpecInfo(
-            name="Output", fields=output_fields, bases=(BaseSpec,)
-        )
+        self.input_spec = input_spec or SpecInfo(name="Inputs", fields=input_fields, bases=(BaseSpec,))
+        self.output_spec = output_spec or SpecInfo(name="Output", fields=output_fields, bases=(BaseSpec,))
         self.output_spec = output_from_inputfields(self.output_spec, self.input_spec)
+
         super().__init__(
             name=name,
             inputs=kwargs,
@@ -94,34 +57,26 @@ class DataladInterface(TaskBase):
         )
 
     def _run_task(self):
+        import datalad.api as dl
+
         in_file = self.inputs.in_file
         dataset_path = self.inputs.dataset_path
 
-        _dl_found = False
-        try:
-            import datalad.api as dl
-
-            _dl_found = True
-        except:
-            raise ImportError("Datalad is not installed.")
-
-        # checking if the dataset is already downloaded
-
+        # Check if the dataset is already downloaded
         if not (Path(dataset_path) / ".datalad").exists():
-            logger.info("Datalad interface without dataset path defined.")
             try:
                 dataset_url = self.inputs.dataset_url
                 os.makedirs(dataset_path, exist_ok=True)
                 dl.install(source=dataset_url, path=dataset_path)
             except Exception as e:
-                logger.error(e)
+                raise e
         else:
             ds = dl.Dataset(self.inputs.dataset_path)
 
-        # getting the file
+        # Get the file
         ds.get(self.inputs.in_file)
 
-        # checking if the file was downloaded
+        # Check if the file was downloaded
         if not Path(dataset_path, in_file).exists():
             raise FileNotFoundError(f"File {in_file} not found in {dataset_path}")
 
@@ -130,19 +85,17 @@ class DataladInterface(TaskBase):
             _pth = dataset_path / _pth
 
         _datalad_candidate = _pth.is_symlink() and not _pth.exists()
-        if not _datalad_candidate:
-            logger.warning("datalad was required but not found")
 
         if _datalad_candidate:
             try:
                 result = dl.get(_pth, dataset=dataset_path)
             except Exception as exc:
-                logger.warning(f"datalad get on {_pth} failed.")
+                raise exc
             else:
                 if result[0]["status"] == "error":
-                    logger.warning(f"datalad get failed: {result}")
+                    raise RuntimeError(f"datalad get failed: {result}")
 
-        self.output_ = None
+                self.output_ = None
         output = os.path.abspath(
             os.path.join(self.inputs.dataset_path, self.inputs.in_file)
         )
@@ -150,7 +103,6 @@ class DataladInterface(TaskBase):
         if output is None:
             self.output_ = {nm: None for nm in output_names}
         elif len(output_names) == 1:
-            # if only one element in the fields, everything should be returned together
             self.output_ = {output_names[0]: output}
         elif isinstance(output, tuple) and len(output_names) == len(output):
             self.output_ = dict(zip(output_names, output))
@@ -159,8 +111,6 @@ class DataladInterface(TaskBase):
                 f"expected {len(self.output_spec.fields)} elements, "
                 f"but {output} were returned"
             )
-        # outputs = self.output_spec().get()
-        # outputs["out_file"] = os.path.abspath(os.path.join(self.inputs.dataset_path, self.inputs.in_file))
 
     def _list_outputs(self):
         outputs = self.output_spec().get()
@@ -168,3 +118,4 @@ class DataladInterface(TaskBase):
             os.path.join(self.inputs.dataset_path, self.inputs.in_file)
         )
         return outputs
+
--- pydra/tasks/datalad/tests/test_interface.py
+++ pydra/tasks/datalad/tests/test_interface.py
@@ -1,39 +1,40 @@
+import os
 import typing as ty
 from pathlib import Path
-import os
+import datalad.api as dl
 from ..interface.datalad import DataladInterface
 from .utils import need_gitannex
 
+
 @need_gitannex
-def test_datalad_interface(tmpdir):
+def test_datalad_interface(tmpdir: Path):
     """
     Testing datalad interface
     """
-    import datalad.api as dl
-
-    # change PosixPath to str
+    # Convert PosixPath to str and create a dataset
     tmpdir = str(tmpdir)
-    # creating a dataset
     ds = dl.Dataset(tmpdir).create()
     ds.save()
     ds_path = ds.pathobj
 
-    # creating a file to download
+    # Create a file to download
     file_path = ds_path / "file.txt"
     file_path.write_text("test")
     ds.save()
 
+    # Convert the tmpdir back to Path
     tmpdir = Path(tmpdir)
 
-    # install the dataset to a new location
+    # Install the dataset to a new location
     ds2 = dl.install(source=tmpdir, path=tmpdir / "ds2")
     ds2_path = ds2.pathobj
 
-    # use datalad interface to download the file
+    # Use datalad interface to download the file
     dl_interface = DataladInterface(
         name="dl_interface", in_file="file.txt", dataset_path=ds2_path
     )
-    # running the task
+
+    # Run the task
     res = dl_interface()
 
     assert os.path.exists(res.output.out_file)
